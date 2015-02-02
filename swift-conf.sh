35dc36a6393ea904804d533574ec141931036610
JetChars <calebjay@live.cn>
2015-02-02 14:30:46 +0800

    previous final version of swift config file at cnode

--- swift-conf.sh
+++ swift-conf.sh
@@ -14,29 +14,16 @@ umask 022
 PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
 FILES=/opt/stack/devstack/files
 DEST=${DEST:-/opt/stack}
-# Keep track of the devstack directory
 TOP_DIR=$(cd $(dirname "$0") && pwd)
+SWIFT_HASH=66a3d6b56c1f479c8b4e70ab5c2000f5
 
-# Import common functions
 source $TOP_DIR/functions
-
-# Import config functions
 source $TOP_DIR/lib/config
-
-# Use openrc + stackrc + localrc for settings
 source $TOP_DIR/stackrc
-
-# Import common services (database, message queue) configuration
 source $TOP_DIR/lib/database
 source $TOP_DIR/lib/rpc_backend
-
-# Import apache functions
 source $TOP_DIR/lib/apache
-
-# Import TLS functions
 source $TOP_DIR/lib/tls
-
-# Source project function libraries
 source $TOP_DIR/lib/infra
 source $TOP_DIR/lib/oslo
 source $TOP_DIR/lib/stackforge
@@ -51,12 +38,19 @@ source $TOP_DIR/lib/heat
 source $TOP_DIR/lib/neutron
 source $TOP_DIR/lib/ldap
 source $TOP_DIR/lib/dstat
-
-# Destination path for installation ``DEST``
-
 source $TOP_DIR/openrc admin admin
 
+cleanup_swift
 install_swift
 configure_swift
-#init_swift
 create_swift_disk
+
+#swift-init --run-dir=${SWIFT_DATA_DIR}/run all restart || true
+sudo /etc/init.d/rsync restart
+#echo ${SWIFT_DATA_DIR} $SWIFT_DIR ${SWIFT_CONF_DIR}
+#local type
+#for type in object container account; do
+    #swift-init --run-dir=${SWIFT_DATA_DIR}/run ${type} stop || true
+    #run_process s-${type} "$SWIFT_DIR/bin/swift-${type}-server ${SWIFT_CONF_DIR}/${type}-server/1.conf -v"
+#done
+sudo swift-init all start
