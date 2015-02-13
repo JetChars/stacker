fad3c79c9b54aaa18e2506295d682aa9c83df1e9
JetChars <calebjay@live.cn>
2015-02-13 13:11:12 +0800

    revision swift-master

--- swift-ring-conf.sh
+++ swift-ring-conf.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 rm -f *.builder *.ring.gz backups/*.builder backups/*.ring.gz
-$SLAVES="SLAVES"
+
+#slaves must be IPs
+SLAVES="192.168.1.33 192.168.1.34 192.168.1.37 192.168.1.24"
 for i in object container account; do
     swift-ring-builder ${i}.builder create 10 3 1
 done
 
-for j in SLAVES;do
+for j in $SLAVES;do
     swift-ring-builder object.builder add r1z1-${j}:6013/sdb1 100
-done
-
-for j in SLAVES;do
     swift-ring-builder account.builder add r1z1-${j}:6012/sdb1 100
-done
-
-for j in SLAVES;do
     swift-ring-builder container.builder add r1z1-${j}:6011/sdb1 100
 done
 
@@ -22,8 +18,8 @@ for i in object container account; do
     swift-ring-builder ${i}.builder rebalance
 done
 
-for j in SLAVES;do
-    scp *.gz stack@$j:/ect/swift
+for j in $SLAVES;do
+    scp *.gz $j:/ect/swift
 done
 
 sudo swift-init all restart
