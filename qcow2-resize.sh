c7e8d5881ddea8f7f8209b6d4f4fbad34e85c504
JetChars <calebjay@live.cn>
2015-03-30 14:55:15 +0800

    add automatic upload func

--- qcow2-resize.sh
+++ qcow2-resize.sh
@@ -4,3 +4,5 @@
 qemu-img create -f qcow2 $2 $3
 virt-resize -d --expand /dev/sda1 $1 $2
 qemu-img info $2
+. /opt/stack/devstack/openrc admin admin
+glance image-create --name=`basename $2 .qcow2` --store=file --disk-format=qcow2 --file=$2 --is-public=True --container-format=bare
