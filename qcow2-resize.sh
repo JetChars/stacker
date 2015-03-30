8148fb847bd89ee78514c82715385b62dd41cce1
JetChars <calebjay@live.cn>
2015-03-30 15:10:54 +0800

    add example cmd

--- qcow2-resize.sh
+++ qcow2-resize.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+##################################################
 # this script is for resize qcow2 image
+##################################################
+# example: ./qcow2-resize.sh centos_sahara_vanilla_hadoop_2_6_latest.selinux-permissive.fixlib.qcow2 tmp.qcow2 100G
+##################################################
+
 # sudo apt-get install libguestfs-tools -y --force-yes 2>/dev/null || sudo yum install -y libguestfs-tools
 qemu-img create -f qcow2 $2 $3
 virt-resize -d --expand /dev/sda1 $1 $2
