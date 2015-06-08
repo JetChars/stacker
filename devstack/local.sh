ea875cccec4ae4d6847a4d04e93f335c1a62015a
JetChars <calebjay@live.cn>
2015-06-08 11:18:43 +0800

    extract variable

--- devstack/local.sh
+++ devstack/local.sh
@@ -1,6 +1,8 @@
+DEFAULT_DISK_SIZE=
+
 . /opt/stack/devstack/openrc admin admin
-glance image-create --name=centos_sahara_vanilla_hadoop_2_6_latest.selinux-permissive.fixlib-500 --store=file --disk-format=qcow2 --file=centos_sahara_vanilla_hadoop_2_6_latest.selinux-permissive.fixlib-500G.qcow2 --is-public=True --container-format=bare --min-disk=500
-nova flavor-create F500 11 81920 500 40
-nova flavor-create F500h 12 40960 500 20
-nova flavor-create F500q 13 20480 500 10
-nova boot --flavor=F500q --image=centos_sahara_vanilla_hadoop_2_6_latest.selinux-permissive.fixlib-500 test
+glance image-create --name=centos_sahara_vanilla_hadoop_2_6_latest.selinux-permissive.fixlib-${DEFAULT_DISK_SIZE} --store=file --disk-format=qcow2 --file=centos_sahara_vanilla_hadoop_2_6_latest.selinux-permissive.fixlib${DEFAULT_DISK_SIZE}.qcow2 --is-public=True --container-format=bare --min-disk=${DEFAULT_DISK_SIZE}
+#nova flavor-create F${DEFAULT_DISK_SIZE} 11 81920 ${DEFAULT_DISK_SIZE} 40
+#nova flavor-create F${DEFAULT_DISK_SIZE}h 12 40960 ${DEFAULT_DISK_SIZE} 20
+#nova flavor-create F${DEFAULT_DISK_SIZE}q 13 20480 ${DEFAULT_DISK_SIZE} 10
+nova boot --flavor=F${DEFAULT_DISK_SIZE}q --image=centos_sahara_vanilla_hadoop_2_6_latest.selinux-permissive.fixlib-${DEFAULT_DISK_SIZE} test
